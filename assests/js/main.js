ca320f51637aaf3099786116f50ac829be30e68d
Felipe <fmp.ti@outlook.com>
2023-03-08 10:21:59 -0300

    :sparkles: feat: CRUD - Função Create

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,9 +1,27 @@
 const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+    .classList.add('active');
 
 const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+    .classList.remove('active');
 
+const tempCliente ={
+    nome: "Luciana",
+    email: "Lua@gmail.com",
+    celular: "(71) 9 9999-9999",
+    cidade: "São Paulo"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('dbCliente')) ?? [];
+const setLocalStorage = (dbCliente) => localStorage.setItem("dbCliente", JSON.stringify(dbCliente));
+
+//CRUD - CREATE
+const criarCliente = (cliente) => {
+    const dbCliente = getLocalStorage();
+    dbCliente.push (cliente)
+    setLocalStorage(dbCliente);
+} 
+
+//Eventos    
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
